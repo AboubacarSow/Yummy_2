872d9ec0624a0c1b6032ccf41b3b9bbf41e9d4be
ABOUBACAR <175620651+AboubacarSow@users.noreply.github.com>
2024-12-31 14:56:53 +0300

    Contat update

--- Controllers/ChefSocialMediaController.cs
+++ Controllers/ChefSocialMediaController.cs
@@ -17,14 +17,60 @@ namespace Yummy_2.Controllers
         }
         public ActionResult Create()
         {
+            GetChefs();
             return View();
         }
         [HttpPost]
         public ActionResult Create(ChefSocial social)
         {
+            if (!ModelState.IsValid)
+            {
+                GetChefs() ;    
+                ModelState.AddModelError("", "Bir hata oluşmuştur");
+                return View(social);
+            }
+            social.Chef=_dbContext.Chefs.FirstOrDefault(c=>c.ChefId == social.ChefId);
             _dbContext.ChefSocials.Add(social);
             _dbContext.SaveChanges();
             return RedirectToAction("Index");
+        }
+        private void GetChefs() 
+        {
+            ViewBag.Chefs=(from c in _dbContext.Chefs.ToList()
+                           select new SelectListItem
+                           {
+                               Text= c.FullName,
+                               Value=c.ChefId.ToString()
+                           }).ToList();
+
+        }
+        public ActionResult Update(int id)
+        {
+            GetChefs();
+            return View(_dbContext.ChefSocials.Find(id));  
+            
+        }
+        public ActionResult Update(ChefSocial social)
+        {
+            if (!ModelState.IsValid)
+            {
+                GetChefs();
+                ModelState.AddModelError("", "Bir hata oluştu");
+                return View(social);    
+            }
+            var value=_dbContext.ChefSocials.FirstOrDefault(x=>x.Id==social.Id);
+            value.Name = social.Name;
+            value.Icon = social.Icon;
+            value.Url = social.Url;
+            value.Chef = _dbContext.Chefs.First(c => c.ChefId == social.ChefId);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            _dbContext.ChefSocials.Remove(_dbContext.ChefSocials.Find(id));
+            _dbContext.SaveChanges();
+            return View("Index");
         }
     }
 }
---
+++ Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Yummy_2.Models;
+
+namespace Yummy_2.Controllers
+{
+    public class ContactController : Controller
+    {
+        YummyDbContext _dbContext= new YummyDbContext();    
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View(_dbContext.ContactInfos.ToList());
+        }
+        public ActionResult Delete(int id)
+        {
+            _dbContext.ContactInfos.Remove(_dbContext.ContactInfos.Find(id));
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");   
+        }
+    }
+}
